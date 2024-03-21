fae0408e6a3d0e2db3dbb372bcfc8aab6aa75ddc
Adam Cooper <adambco@me.com>
2024-03-21 07:55:46 -0700

    Update keywords

--- src/gatsby-ssr.js
+++ src/gatsby-ssr.js
@@ -1,16 +1,25 @@
-import React from 'react';
+import React from "react";
 
-exports.onRenderBody = ({setPostBodyComponents}, pluginOptions) => {
-    return setPostBodyComponents([
-      <script
-        key={'gatsby-plugin-support-bubble'}
-        dangerouslySetInnerHTML={{
-          __html: `
-          window.supportBubbleClientId = '${pluginOptions.clientId}';
-          window.supportBubbleClientToken = '${pluginOptions.clientToken}';
-          !function(){var t;if(!(t=window.supportBubblet=window.supportBubble=window.supportBubblet||[]).init)t.invoked?window.console&&console.error&&console.error("Support Bubble snippet included twice."):(t.invoked=!0,t.methods=["identify","config","track","reset","debug","show","ping","page","hide","off","on"],t.factory=function(e){return function(){var n;return(n=Array.prototype.slice.call(arguments)).unshift(e),t.push(n),t}},t.methods.forEach(function(e){t[e]=t.factory(e)}),t.load=function(t){var e,n,o;o=3e5*Math.ceil(new Date/3e5),(n=document.createElement("script")).type="text/javascript",n.async=!0,n.crossorigin="anonymous",n.src="https://d2huomdiop2bs1.cloudfront.net/chat-module.js",(e=document.getElementsByTagName("script")[0]).parentNode.insertBefore(n,e)})}();
-          `,
-        }}
-      />,
-    ]);
+const onRenderBody = ({ setPostBodyComponents }, pluginOptions) => {
+  console.log('pluginOptions', pluginOptions);
+  const { clientID, clientToken } = pluginOptions;
+  return setPostBodyComponents([
+    <script
+      key={'gatsby-plugin-support-bubble'}
+      dangerouslySetInnerHTML={{
+        __html: `
+          (function() {
+            var script = document.createElement('script');
+            script.setAttribute('clientID', '${clientID}');
+            script.setAttribute('clientToken', '${clientToken}');
+            script.src = 'https://d2huomdiop2bs1.cloudfront.net/chat-module.js';
+            script.async = true;
+            document.body.appendChild(script);
+          })();
+        `,
+      }}
+    />,
+  ]);
 };
+
+export { onRenderBody };
