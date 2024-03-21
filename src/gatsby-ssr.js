c9149e0ae6d1856f0f99e8235a5fbae59154c753
Adam Cooper <adambco@me.com>
2024-03-21 10:25:39 -0700

    Update

--- src/gatsby-ssr.js
+++ src/gatsby-ssr.js
@@ -3,22 +3,23 @@ import React from "react";
 const onRenderBody = ({ setPostBodyComponents }, pluginOptions) => {
   console.log('pluginOptions', pluginOptions);
   const { clientID, clientToken } = pluginOptions;
+
+  const scriptContent = `
+    (function() {
+      var script = document.createElement('script');
+      script.setAttribute('clientID', '${clientID}');
+      script.setAttribute('clientToken', '${clientToken}');
+      script.src = 'https://d2huomdiop2bs1.cloudfront.net/chat-module.js';
+      script.async = true;
+      document.body.appendChild(script);
+    })();
+  `;
+
   return setPostBodyComponents([
-    <script
-      key={'gatsby-plugin-support-bubble'}
-      dangerouslySetInnerHTML={{
-        __html: `
-          (function() {
-            var script = document.createElement('script');
-            script.setAttribute('clientID', '${clientID}');
-            script.setAttribute('clientToken', '${clientToken}');
-            script.src = 'https://d2huomdiop2bs1.cloudfront.net/chat-module.js';
-            script.async = true;
-            document.body.appendChild(script);
-          })();
-        `,
-      }}
-    />,
+    React.createElement('script', {
+      key: 'gatsby-plugin-support-bubble',
+      dangerouslySetInnerHTML: { __html: scriptContent }
+    })
   ]);
 };
 
